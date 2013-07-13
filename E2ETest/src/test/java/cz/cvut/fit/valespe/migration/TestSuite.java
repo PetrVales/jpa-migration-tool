0763b09142470413557f0af8455636d359976a73
Bc. Petr Valeš <valespe3@fit.cvut.cz>
2013-07-13 22:50:47 +0200

    Test suite start add-ons, run tests, uninstall add-ons

--- E2ETest/src/test/java/cz/cvut/fit/valespe/migration/TestSuite.java
+++ E2ETest/src/test/java/cz/cvut/fit/valespe/migration/TestSuite.java
@@ -1,12 +1,13 @@
 package cz.cvut.fit.valespe.migration;
 
 import junit.framework.TestCase;
+import org.apache.commons.io.IOUtils;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-import java.io.File;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,22 +15,106 @@ import java.util.Map;
 @Suite.SuiteClasses({RunRooScriptTest.class, InitProjectTest.class, MigrationSetupTest.class})
 public class TestSuite extends TestCase {
 
-
-
     private static Map<String, File> ADDONS = new HashMap<String, File>();
 
-    static {
+    private static String UNINSTALL_SCRIPT = "uninstallAddon";
+    private static String INSTALL_SCRIPT = "startAddon";
+    private static String ADD_ON_NAME = "{addonName}";
+    private static String ADD_ON_URL = "{addonUrl}";
+    private static File INSTALL_DIRECTORY = new File("/tmp/");
 
+    // FIXME this is ugly, but i have no better idea... feel free to improve
+    static {
+        ADDONS.put("cz.cvut.valespe.migration.setup", new File("/home/petr/workspace/diplomka/jpa-refactoring-tool/migrationSetup"));
     }
 
     @BeforeClass
-    public static void doYourOneTimeSetup() {
+    public static void beforeTests() throws IOException, InterruptedException {
+        for (Map.Entry<String, File> entry : ADDONS.entrySet()) {
+            mvnClean(entry.getValue());
+            mvnPackage(entry.getValue());
+            rooUninstall(entry.getKey());
+            rooInstall(new File(entry.getValue(), "target/" + entry.getKey() + "-0.1.0.BUILD-SNAPSHOT.jar"));
+        }
+    }
 
+    private static void mvnClean(File location) throws IOException, InterruptedException {
+        executeMvnCommand(location, "clean");
+    }
+
+    private static void mvnPackage(File location) throws IOException, InterruptedException {
+        executeMvnCommand(location, "package");
+    }
+
+    private static void executeMvnCommand(File location, String command) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c",  "mvn " + command, "-c");
+        processBuilder.directory(location);
+        Process process = processBuilder.start();
+        if (process.waitFor() != 0) {
+            throw new IllegalStateException("Failed to run mvn " + command + " on project: " + location.toString());
+        }
+    }
+
+    private static void rooUninstall(String addonName) throws IOException, InterruptedException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put(ADD_ON_NAME, addonName);
+        prepareScript(UNINSTALL_SCRIPT, params);
+        runRooScript(UNINSTALL_SCRIPT);
+        removeScript(UNINSTALL_SCRIPT);
+    }
+
+    private static void rooInstall(File addonLocation) throws IOException, InterruptedException {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put(ADD_ON_URL, addonLocation.getAbsolutePath());
+        prepareScript(INSTALL_SCRIPT, params);
+        runRooScript(INSTALL_SCRIPT);
+        removeScript(INSTALL_SCRIPT);
+    }
+
+    private static void prepareScript(String scriptName, Map<String, String> params) throws IOException {
+        copyScript(scriptName, getScriptText(scriptName, params));
+    }
+
+    private static String getScriptText(String scriptName, Map<String, String> params) throws IOException {
+        String scriptText = IOUtils.toString(TestSuite.class.getResourceAsStream(scriptName));
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            scriptText = scriptText.replace(entry.getKey(), entry.getValue());
+        }
+        return scriptText;
+    }
+
+    private static void copyScript(String scriptName, String scriptText) throws IOException {
+        InputStream script = IOUtils.toInputStream(scriptText);
+        File scriptFile = new File(INSTALL_DIRECTORY, scriptName);
+        if (scriptFile.createNewFile()) {
+            FileOutputStream stream = new FileOutputStream(scriptFile);
+            IOUtils.copy(script, stream);
+            stream.close();
+        }
+        script.close();
+    }
+
+    private static void runRooScript(String scriptName) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder("bash", "-c",  "roo < " + scriptName, "-c");
+        processBuilder.directory(INSTALL_DIRECTORY);
+        Process process = processBuilder.start();
+        if (process.waitFor() != 0) {
+            throw new IllegalStateException("Script to install addon: " + scriptName);
+        }
+    }
+
+    private static void removeScript(String scriptName) throws IOException {
+        File scriptFile = new File(INSTALL_DIRECTORY, scriptName);
+        if (scriptFile.exists()) {
+            scriptFile.delete();
+        }
     }
 
     @AfterClass
-    public static void doYourOneTimeTeardown() {
-
+    public static void afterTests() throws IOException, InterruptedException {
+        for (Map.Entry<String, File> entry : ADDONS.entrySet()) {
+            rooUninstall(entry.getKey());
+        }
     }
 
 }
