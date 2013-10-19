06e7780def6c426f89fe8a533739f683a35b587a
Bc. Petr Valeš <valespe3@fit.cvut.cz>
2013-10-19 20:03:56 +0200

    Redesign to single addon

---
+++ migration/src/main/java/cz/cvut/fit/valespe/migration/operation/MergeClassOperations.java
@@ -0,0 +1,11 @@
+package cz.cvut.fit.valespe.migration.operation;
+
+import org.springframework.roo.classpath.details.ClassOrInterfaceTypeDetails;
+import org.springframework.roo.model.JavaType;
+
+public interface MergeClassOperations {
+
+    void mergeClasses(JavaType target, JavaType classA, JavaType classB, String table, String schema, String catalog, String tablespace);
+
+    void createTable(ClassOrInterfaceTypeDetails targetTypeDetails);
+}
---
+++ migration/src/main/java/cz/cvut/fit/valespe/migration/operation/SplitClassOperations.java
@@ -0,0 +1,15 @@
+package cz.cvut.fit.valespe.migration.operation;
+
+import org.springframework.roo.classpath.details.FieldMetadata;
+import org.springframework.roo.model.JavaType;
+
+import java.util.List;
+
+public interface SplitClassOperations {
+
+    void createClass(JavaType original, JavaType target, List<FieldMetadata> propertiesA, String table, String schema, String catalog, String tablespace);
+
+    void removeClass(JavaType target);
+
+    void createTable(List<FieldMetadata> properties, String table, String schema, String catalog, String tablespace);
+}
